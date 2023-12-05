d5807d4d3c3f37d88c02199c4c3bd0fd4363eecd
Bisi3asi <kueeng8008@naver.com>
2023-12-05 21:49:55 +0900

    feat: Comment 엔티티 추가

---
+++ medium/src/main/java/com/example/medium/domain/comment/entity/Comment.java
@@ -0,0 +1,24 @@
+package com.example.medium.domain.comment.entity;
+
+import com.example.medium.domain.member.entity.Member;
+import com.example.medium.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(callSuper = true)
+public class Comment extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member author;
+
+    @Column(nullable = false, length = 300)
+    private String content;
+}
