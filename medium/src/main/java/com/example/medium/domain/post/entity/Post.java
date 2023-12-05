44bb1b0607b7c16c01b8b8a555a9e24ed968bd51
Bisi3asi <kueeng8008@naver.com>
2023-12-05 20:53:54 +0900

    feat, rename: Post 엔티티 추가, 디렉토리 이름 변경

---
+++ medium/src/main/java/com/example/medium/domain/post/entity/Post.java
@@ -0,0 +1,9 @@
+package com.example.medium.domain.post.entity;
+
+import com.example.medium.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Post extends BaseEntity {
+
+}
