be05173a7b2bdedd61b2e73e20ae4f6b36c0cd8b
Bisi3asi <kueeng8008@naver.com>
2023-12-05 22:20:04 +0900

    fix: (Member) email, nickname 제거

--- medium/src/main/java/com/example/medium/domain/member/entity/Member.java
+++ medium/src/main/java/com/example/medium/domain/member/entity/Member.java
@@ -3,7 +3,6 @@ package com.example.medium.domain.member.entity;
 import com.example.medium.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,11 +21,4 @@ public class Member extends BaseEntity {
 
     @Column(nullable = false)
     private String password;
-
-    @Email
-    @Column(nullable = false, length = 30)
-    private String email;
-
-    @Column(nullable = false, length = 20)
-    private String nickname;
 }
