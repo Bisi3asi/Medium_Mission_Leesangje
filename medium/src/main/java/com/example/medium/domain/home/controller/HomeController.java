cfcc015550ac829cc1c7fd1f697816c3170c9bfc
Bisi3asi <kueeng8008@naver.com>
2023-12-05 22:50:41 +0900

    comment: todo 추가

--- medium/src/main/java/com/example/medium/domain/home/controller/HomeController.java
+++ medium/src/main/java/com/example/medium/domain/home/controller/HomeController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    // Get : / * 최신글 30개 노출
     @GetMapping("/")
     public String showMain(){
         return "domain/home/main";
