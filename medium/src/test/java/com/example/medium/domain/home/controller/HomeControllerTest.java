d94ff528618e339fc3945175d2749d1a5d930960
Bisi3asi <kueeng8008@naver.com>
2023-12-05 20:51:35 +0900

    feat: 메인 추가 및 컨트롤러 초기 테스트 완료

---
+++ medium/src/test/java/com/example/medium/domain/home/controller/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.medium.domain.home.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("메인")
+    @Test
+    void 메인_테스트1() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform((get("/")))
+                .andDo(print());
+    }
+}
