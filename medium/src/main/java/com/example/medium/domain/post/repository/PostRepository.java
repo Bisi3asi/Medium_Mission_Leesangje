320ec76352c82b7291b5def8f990723136bdbf97
Bisi3asi <kueeng8008@naver.com>
2023-12-06 22:50:03 +0900

    feat: Post findAll 메소드 추가

--- medium/src/main/java/com/example/medium/domain/post/repository/PostRepository.java
+++ medium/src/main/java/com/example/medium/domain/post/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.example.medium.domain.post.repository;
 
 import com.example.medium.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Long, Post> {
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAll(Pageable pageable);
 }
