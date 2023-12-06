10c3ec2622503081bac6b8114fb081c743250c73
Bisi3asi <kueeng8008@naver.com>
2023-12-06 22:50:28 +0900

    feat: Post getList(Page) 추가

--- medium/src/main/java/com/example/medium/domain/post/service/PostService.java
+++ medium/src/main/java/com/example/medium/domain/post/service/PostService.java
@@ -1,13 +1,29 @@
 package com.example.medium.domain.post.service;
 
+import com.example.medium.domain.post.entity.Post;
 import com.example.medium.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
     private final PostRepository postRepository;
+
+    public Page<Post> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("id"));
+
+        Pageable pageable = PageRequest.of(page, 30, Sort.by(sorts));
+        return postRepository.findAll(pageable);
+    }
 }
