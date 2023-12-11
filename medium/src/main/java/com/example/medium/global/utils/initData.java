3afafabcc5b913a32c248cb3313e695e3123f3fd
Bisi3asi <kueeng8008@naver.com>
2023-12-12 01:55:00 +0900

    chore: initData 초기 설정 후 !dev로 수정, ddl-auto update

--- medium/src/main/java/com/example/medium/global/utils/initData.java
+++ medium/src/main/java/com/example/medium/global/utils/initData.java
@@ -2,11 +2,14 @@ package com.example.medium.global.utils;
 
 import com.example.medium.domain.comment.entity.Comment;
 import com.example.medium.domain.comment.repository.CommentRepository;
-import com.example.medium.domain.member.entity.Member;
-import com.example.medium.domain.member.repository.MemberRepository;
+import com.example.medium.domain.member.dto.MemberRequestDto;
+import com.example.medium.domain.member.service.MemberService;
+import com.example.medium.domain.post.dto.PostRequestDto;
 import com.example.medium.domain.post.entity.Post;
-import com.example.medium.domain.post.repository.PostRepository;
+import com.example.medium.domain.post.service.PostService;
+import com.example.medium.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -14,53 +17,56 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
-@Profile("!prod")
+@Profile("!dev")
 @Configuration
 @RequiredArgsConstructor
 public class initData {
-    private final PostRepository postRepository;
+    private final PostService postService;
     private final CommentRepository commentRepository;
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
     @Bean
     public ApplicationRunner run() {
-        return new ApplicationRunner() {
+        return new ApplicationRunner(){
             @Override
             @Transactional
-            public void run(ApplicationArguments args) throws Exception {
-                Member member = Member.builder()
-                        .username("sbbadmin")
-                        .password("12345678")
-                        .build();
-                memberRepository.save(member);
-
-                member = Member.builder()
-                        .username("testuser1")
-                        .password("12345678")
-                        .build();
-                memberRepository.save(member);
+            @SneakyThrows
+            public void run(ApplicationArguments args) {
+                memberService.create(new MemberRequestDto(
+                        "sbbadmin"
+                        , "12345678",
+                        "12345678")
+                );
+                memberService.create(new MemberRequestDto(
+                        "testuser1"
+                        , "12345678",
+                        "12345678")
+                );
 
                 for (int i = 1; i < 100; i++) {
-                    Post post = (i % 2 == 0) ?
-                            Post.builder()
-                                    .title(String.format("테스트 글 %d", i))
-                                    .content(String.format("테스트 내용 %d", i))
-                                    .author(member)
-                                    .isPublished(false)
-                                    .build()
-                            :
-                            Post.builder()
-                                    .title(String.format("테스트 글 %d", i))
-                                    .content(String.format("테스트 내용 %d", i))
-                                    .author(member)
-                                    .isPublished(true)
-                                    .build();
-                    postRepository.save(post);
-
+                    ResponseDto<Post> postResp;
+                    if (i % 2 == 0) {
+                        postResp = postService.create(new PostRequestDto(
+                                        memberService.findByUsername("testuser1"),
+                                        false,
+                                        String.format("테스트 글 %d", i),
+                                        String.format("테스트 내용 %d", i)
+                                )
+                        );
+                    } else {
+                        postResp = postService.create(new PostRequestDto(
+                                        memberService.findByUsername("testuser1"),
+                                        true,
+                                        String.format("테스트 글 %d", i),
+                                        String.format("테스트 내용 %d", i)
+                                )
+                        );
+                    }
                     for (int j = 0; j < 3; j++) {
+                        Post post = postResp.getData();
                         Comment comment = Comment.builder()
                                 .post(post)
-                                .author(member)
+                                .author(post.getAuthor())
                                 .content(String.format("테스트 댓글 %d", i))
                                 .build();
                         commentRepository.save(comment);
