c5502c37effd9c1a35d01153b01f828502acae5b
Bisi3asi <kueeng8008@naver.com>
2023-12-12 01:57:10 +0900

    fix: PostRequestDto validation, 생성자 수정

--- medium/src/main/java/com/example/medium/domain/post/dto/PostRequestDto.java
+++ medium/src/main/java/com/example/medium/domain/post/dto/PostRequestDto.java
@@ -3,12 +3,12 @@ package com.example.medium.domain.post.dto;
 import com.example.medium.domain.member.entity.Member;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class PostRequestDto {
     private Member author;
 
@@ -19,6 +19,6 @@ public class PostRequestDto {
     private String title;
 
     @NotBlank(message = "내용을 입력해주세요.")
-    @Size(max = 50, message = "내용은 1000자를 초과할 수 없습니다.")
+    @Size(max = 1000, message = "내용은 1000자를 초과할 수 없습니다.")
     private String content;
 }
