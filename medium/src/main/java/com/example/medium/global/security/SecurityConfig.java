1d72b5ecf91e6b0245ef177afd6e196600fbe73d
Bisi3asi <kueeng8008@naver.com>
2023-12-12 01:56:44 +0900

    feat: passwordEncoder Bean 등록

--- medium/src/main/java/com/example/medium/global/security/SecurityConfig.java
+++ medium/src/main/java/com/example/medium/global/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.medium.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,5 +31,10 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
