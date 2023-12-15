63481123d92bff5c3171214ebe57819d5163c51d
Bisi3asi <kueeng8008@naver.com>
2023-12-15 09:41:26 +0900

    feat: memberService id/비밀번호 검증, jwt 토큰 기반 멤버 조회 기능 구현

--- medium/src/main/java/com/example/medium/domain/member/service/MemberService.java
+++ medium/src/main/java/com/example/medium/domain/member/service/MemberService.java
@@ -4,13 +4,21 @@ import com.example.medium.domain.member.dto.MemberRequestDto;
 import com.example.medium.domain.member.entity.Member;
 import com.example.medium.domain.member.repository.MemberRepository;
 import com.example.medium.global.dto.ResponseDto;
+import com.example.medium.global.security.SecurityUser;
+import com.example.medium.global.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,18 +27,18 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public Member findByUsername(String username){
+    public Member findByUsername(String username) {
         return memberRepository.findByUsername(username).orElseThrow(
-            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Member not found")
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Member not found")
         );
     }
 
     @Transactional
-    public ResponseDto<Member> create(MemberRequestDto memberRequestDto){
-        if (!memberRequestDto.getPassword().equals(memberRequestDto.getPasswordConfirm())){
+    public ResponseDto<Member> create(MemberRequestDto memberRequestDto) {
+        if (!memberRequestDto.getPassword().equals(memberRequestDto.getPasswordConfirm())) {
             throw new IllegalArgumentException("please check out password, password confirm.");
         }
-        if (memberRepository.findByUsername(memberRequestDto.getUsername()).isPresent()){
+        if (memberRepository.findByUsername(memberRequestDto.getUsername()).isPresent()) {
             throw new DuplicateKeyException("username already exists, please choose another one.");
         }
 
@@ -42,5 +50,31 @@ public class MemberService {
 
         memberRepository.save(member);
         return ResponseDto.of("200", "you have successfully joined", member);
+    }
+
+    public SecurityUser getUserFromAccessToken(String accessToken) {
+        Claims claims = JwtUtil.decode(accessToken);
+
+        Map<String, Object> data = (Map<String, Object>) claims.get("data");
+        long id = Long.parseLong((String) data.get("id"));
+        String username = (String) data.get("username");
+        List<? extends GrantedAuthority> authorities =
+                ((List<String>) data.get("authorities")).stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList();
+
+        return new SecurityUser(id, username, "", authorities);
+    }
+
+    public ResponseDto<Member> checkUsernameAndPassword(String username, String password) {
+        Member member = memberRepository.findByUsername(username).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "login failed: check Id or password")
+        );
+
+        if(!passwordEncoder.matches(password, member.getPassword())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "login failed: check Id or password");
+        }
+
+        return ResponseDto.of("200", String.format("welcome, %s!", username), member);
     }
 }
