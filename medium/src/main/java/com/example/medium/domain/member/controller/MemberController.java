15830f82e4c91136359d1540b46e84159d2d2976
Bisi3asi <kueeng8008@naver.com>
2023-12-20 01:12:03 +0900

    feat: Jwt 토큰 기반 로그인, 로그아웃 Post 구현

--- medium/src/main/java/com/example/medium/domain/member/controller/MemberController.java
+++ medium/src/main/java/com/example/medium/domain/member/controller/MemberController.java
@@ -1,7 +1,11 @@
 package com.example.medium.domain.member.controller;
 
 import com.example.medium.domain.member.dto.MemberJoinRequestDto;
+import com.example.medium.domain.member.dto.MemberLoginRequestDto;
+import com.example.medium.domain.member.entity.Member;
 import com.example.medium.domain.member.service.MemberService;
+import com.example.medium.global.response.ResponseData;
+import com.example.medium.global.rq.Rq;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,7 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.Objects;
 
 @Controller
@@ -19,6 +25,7 @@ import java.util.Objects;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
     // Get: /member/join
     @GetMapping("/join")
@@ -35,19 +42,21 @@ public class MemberController {
     @PostMapping("/join")
     public String join(@ModelAttribute("memberJoinRequestDto") @Valid
                        MemberJoinRequestDto memberJoinRequestDto,
-                       BindingResult brs) {
+                       BindingResult brs,
+                       RedirectAttributes attr) {
 
         // 1차 : 비밀번호와 비밀번호 확인 일치 검증 (Controller)
-        if (!Objects.equals(memberJoinRequestDto.getPassword(), memberJoinRequestDto.getPasswordConfirm())){
+        if (!Objects.equals(memberJoinRequestDto.getPassword(), memberJoinRequestDto.getPasswordConfirm())) {
             brs.addError(new ObjectError("password", "please check match of the password and password Confirm"));
             return "domain/member/join_form";
         }
         // 2차 : 동일 id 존재 여부 검증(Service)
-        memberService.create(memberJoinRequestDto, brs);
-        if (brs.hasErrors()){
+        ResponseData responseData = memberService.create(memberJoinRequestDto, brs);
+        if (brs.hasErrors()) {
             return "domain/member/join_form";
         }
 
+        attr.addFlashAttribute("msg", responseData.getMsg());
         return "redirect:/member/login";
     }
 
@@ -61,15 +70,49 @@ public class MemberController {
         return "domain/member/login_form";
     }
 
-    // Post: /member/login (Spring Security)
+    // Post: /member/login (JWT token)
     @PostMapping("/login")
-    public String login() {
+    public String login(@ModelAttribute("memberLoginRequestDto") @Valid
+                        MemberLoginRequestDto memberLoginRequestDto,
+                        BindingResult brs,
+                        RedirectAttributes attr) {
+
+        // 1차 : 유효 길이 확인 검증 (DTO)
+        if (brs.hasErrors()) {
+            return "domain/member/login_form";
+        }
+
+        // 2차 : username, password 일치 여부 확인 검증 (service)
+        ResponseData<Member> responseData = memberService.checkUsernameAndPassword(memberLoginRequestDto, brs);
+        if (brs.hasErrors() || responseData.getResultCode().contentEquals("400")) {
+            return "domain/member/login_form";
+        }
+        Member member = responseData.getData();
+
+        // 로그인 시에 accessToken, refreshToken 재생산, DB에도 업데이트
+        String accessToken = memberService.makeToken(member, 10);
+        String refreshToken = memberService.makeToken(member, 60 * 24 * 7);
+        memberService.setRefreshToken(member, refreshToken);
+
+        rq.setAccessTokenToCookie(accessToken);
+        rq.setRefreshTokenToCookie(refreshToken);
+
+        attr.addFlashAttribute("msg", responseData.getMsg());
         return "redirect:/";
     }
 
-    // Post: /member/logout
-    @PostMapping("/logout")
-    public String logout() {
+    // Get: /member/logout
+    @GetMapping("/logout")
+    public String logout(RedirectAttributes attr,
+                         Principal principal) {
+        if (principal == null) {
+            return "redirect:/";
+        }
+
+        if (rq.getAccessTokenFromCookie(null) != null) {
+            rq.removeAccessTokenFromCookie();
+        }
+        attr.addFlashAttribute("msg", "I hope you back soon!");
         return "redirect:/";
     }
 }
