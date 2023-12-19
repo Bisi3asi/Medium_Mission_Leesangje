d760199dda0c99eed8a8c89e2b771dced04dac3e
Bisi3asi <kueeng8008@naver.com>
2023-12-20 01:12:45 +0900

    feat: MemberRepository findByRefreshToken 메소드 추가

--- medium/src/main/java/com/example/medium/domain/member/repository/MemberRepository.java
+++ medium/src/main/java/com/example/medium/domain/member/repository/MemberRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUsername(String username);
+
+    Optional<Member> findByRefreshToken(String refreshToken);
 }
