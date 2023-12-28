0bd9518bb1be453e241f8f99097c87845858a565
Bisi3asi <kueeng8008@naver.com>
2023-12-29 02:51:57 +0900

    refactor: url 기존 member/prime -> prime 이동, 도메인 home으로 종속

--- medium/src/main/java/com/example/medium/domain/home/controller/HomeController.java
+++ medium/src/main/java/com/example/medium/domain/home/controller/HomeController.java
@@ -1,10 +1,49 @@
 package com.example.medium.domain.home.controller;
 
+import com.example.medium.domain.member.entity.Member;
+import com.example.medium.domain.member.service.MemberService;
+import com.example.medium.global.response.ResponseData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("")
 public class HomeController {
+    private final MemberService memberService;
+    @GetMapping("/prime")
+    public String showPrime() {
+        return "domain/home/prime";
+    }
 
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping("/prime/join")
+    public String joinPrime(Principal principal,
+                            RedirectAttributes attr) {
+        if (principal == null) {
+            return "redirect:/";
+        }
+        ResponseData<Member> resp = memberService.setPrime(memberService.findByUsername(principal.getName()));
+        attr.addFlashAttribute("msg", resp.getMsg());
+        return "redirect:/";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping("/prime/cancel")
+    public String cancelPrime(Principal principal,
+                              RedirectAttributes attr) {
+        if (principal == null) {
+            return "redirect:/";
+        }
+        ResponseData<Member> resp = memberService.deletePrime(memberService.findByUsername(principal.getName()));
+        attr.addFlashAttribute("msg", resp.getMsg());
+        return "redirect:/";
+    }
 }
--- medium/src/main/java/com/example/medium/domain/member/controller/MemberController.java
+++ medium/src/main/java/com/example/medium/domain/member/controller/MemberController.java
@@ -115,9 +115,4 @@ public class MemberController {
         attr.addFlashAttribute("msg", "로그아웃 되었습니다.");
         return "redirect:/";
     }
-
-    @GetMapping("/prime")
-    public String showPrime() {
-        return "domain/member/prime";
-    }
 }
