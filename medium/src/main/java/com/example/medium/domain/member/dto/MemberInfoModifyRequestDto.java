3bda466b3f4ac7fb28eb0d6dbeeb6f4cfffa75ad
Bisi3asi <kueeng8008@naver.com>
2024-01-04 02:35:33 +0900

    fix: 에러 메시지 validation 일치화

--- medium/src/main/java/com/example/medium/domain/member/dto/MemberInfoModifyRequestDto.java
+++ medium/src/main/java/com/example/medium/domain/member/dto/MemberInfoModifyRequestDto.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class MemberInfoModifyRequestDto {
-    @NotBlank(message = "유저 이름을 입력해주세요.")
-    @Size(min = 2, max = 20, message = "유저 이름은 8자 ~ 20자로 입력해주세요.")
+    @NotBlank(message = "사용자 이름을 입력해주세요.")
+    @Size(min = 2, max = 20, message = "사용자 이름은 2자 ~ 20자로 입력해주세요.")
     private final String nickname;
 
     @Size(max = 50, message = "프로필 메세지는 50자 이내로 입력해주세요.")
